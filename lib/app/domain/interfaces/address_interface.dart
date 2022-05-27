233e0d36da4ec9f5e122dec7466fea982bc2742b
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-05-27 20:22:24 -0300

    refactor: :recycle: renomeando métodos da interface

--- lib/app/domain/interfaces/address_interface.dart
+++ lib/app/domain/interfaces/address_interface.dart
@@ -1,7 +1,7 @@
 import 'package:eletronic_schedule/app/domain/entities/address_entity.dart';
 
 abstract class AddressInterface {
-  Future<List<Address>> find();
+  Future<List<Address>> getList();
   save(Address address);
   remove(Address address);
 }
--- lib/app/domain/interfaces/contact_interface.dart
+++ lib/app/domain/interfaces/contact_interface.dart
@@ -1,7 +1,7 @@
 import 'package:eletronic_schedule/app/domain/entities/contact_entity.dart';
 
 abstract class ContactInterface{
-    Future<List<Contact>> find();
+    Future<List<Contact>> getList();
     save(Contact contact);
     remove(Contact contact);
 }
