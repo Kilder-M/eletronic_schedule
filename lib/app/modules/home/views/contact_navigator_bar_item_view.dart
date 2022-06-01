8cc56616d54c75578169d89395f72a58706a7611
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-06-01 11:42:23 -0300

    feat: :lipstick: contactBottomBarView criado junto com arquivo de controller

---
+++ lib/app/modules/home/views/contact_navigator_bar_item_view.dart
@@ -0,0 +1,44 @@
+import 'package:eletronic_schedule/app/modules/home/controllers/contact_navigator_bar_item_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ContactNavigatorBarItemView
+    extends GetView<ContactNavigatorBarItemController> {
+  const ContactNavigatorBarItemView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top:40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left:20.0),
+              child: Text(
+                'Contatos',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 2,
+                  itemBuilder: (context, snapshot) {
+                    return const ListTile(
+                      title: Text(
+                        'title',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                      ),
+                      subtitle: Text('subTitle'),
+                      leading:
+                          CircleAvatar(backgroundColor: Colors.blue, radius: 30),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
