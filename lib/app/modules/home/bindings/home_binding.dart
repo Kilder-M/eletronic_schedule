94151b02ba24790b1b53de530f7330eb5d62b539
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-06-01 11:44:56 -0300

    feat: :sparkles: bindings do homecontroller

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,10 +1,19 @@
 import 'package:get/get.dart';
 
+import 'package:eletronic_schedule/app/modules/home/controllers/contact_navigator_bar_item_controller.dart';
+import 'package:eletronic_schedule/app/modules/home/controllers/notification_navigator_bar_item_controller.dart';
+
 import '../controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<NotificationNavigatorBarItemController>(
+      () => NotificationNavigatorBarItemController(),
+    );
+    Get.lazyPut<ContactNavigatorBarItemController>(
+      () => ContactNavigatorBarItemController(),
+    );
     Get.lazyPut<HomeController>(
       () => HomeController(),
     );
