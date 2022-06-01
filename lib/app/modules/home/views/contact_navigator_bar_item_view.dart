82f2c518bb079f8878ea057d8a9eba9b98816330
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-06-01 14:39:00 -0300

    feat: :lipstick: Desing da view finalizado

--- lib/app/modules/home/views/contact_navigator_bar_item_view.dart
+++ lib/app/modules/home/views/contact_navigator_bar_item_view.dart
@@ -1,3 +1,4 @@
+import 'package:eletronic_schedule/app/modules/contact_form/views/contact_form_view.dart';
 import 'package:eletronic_schedule/app/modules/home/controllers/contact_navigator_bar_item_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -9,13 +10,19 @@ class ContactNavigatorBarItemView
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Get.to(const ContactFormView());
+          },
+          backgroundColor: Colors.pink,
+          child: const Icon(Icons.add)),
       body: Padding(
-        padding: const EdgeInsets.only(top:40.0),
+        padding: const EdgeInsets.only(top: 40.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Padding(
-              padding: EdgeInsets.only(left:20.0),
+              padding: EdgeInsets.only(left: 20.0),
               child: Text(
                 'Contatos',
                 style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
@@ -28,11 +35,12 @@ class ContactNavigatorBarItemView
                     return const ListTile(
                       title: Text(
                         'title',
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w500),
                       ),
                       subtitle: Text('subTitle'),
-                      leading:
-                          CircleAvatar(backgroundColor: Colors.blue, radius: 30),
+                      leading: CircleAvatar(
+                          backgroundColor: Colors.blue, radius: 30),
                     );
                   }),
             ),
