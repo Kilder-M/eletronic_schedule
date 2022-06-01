795b70f37e855b5d476ba87fd74f872a909c2e2a
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-06-01 15:17:52 -0300

    feat: :sparkles: contact service criado com todas as requisições

---
+++ lib/app/domain/services/contact_service.dart
@@ -0,0 +1,100 @@
+import 'package:eletronic_schedule/app/data/database/dao/contact_dao_imp.dart';
+import 'package:eletronic_schedule/app/domain/entities/address_entity.dart';
+import 'package:eletronic_schedule/app/domain/entities/contact_entity.dart';
+import 'package:eletronic_schedule/app/domain/exceptions/domain_layer_exception.dart';
+
+class ContactService {
+  final _contactDAO = ContactDAOImp();
+
+  save(Contact contact,Address address) async {
+    validateEmail(contact.email);
+    validatePhone(contact.phone);
+    validateState(address.city, address.state, address.zipCode, address.streetAddress, address.streetAddressNumber, address.complement);
+    validateCity(address.city, address.state, address.zipCode, address.streetAddress, address.streetAddressNumber, address.complement);
+    await _contactDAO.save(contact);
+  }
+
+  remove(Contact contact) async {
+    await _contactDAO.remove(contact);
+  }
+
+  getList() async {
+    await _contactDAO.getList();
+  }
+
+  getByName(String name) async {
+    await _contactDAO.getListByName(name);
+  }
+
+  getByPhone(String phone) async {
+    await _contactDAO.getListByPhone(phone);
+  }
+
+  validateName(String? name) {
+    var max = 50;
+    var min = 2;
+
+    if (name == null) {
+      throw DomainLayerException(message: "O nome é obrigatório.");
+    } else if (name.length > max) {
+      throw DomainLayerException(
+          message: "O nome deve possuir no máximo $max caracters.");
+    } else if (name.length < min) {
+      throw DomainLayerException(
+          message: "O nome deve possuir no mínimo $min caracters.");
+    }
+  }
+
+  validatePhone(String? phone) {
+    var format = RegExp(
+        r'^\([1-9]{2}\) [9] [6-9]{1}[0-9]{3}\-[0-9]{4}$'); // (99) 9 9999-9999
+    if (phone == null) {
+      throw DomainLayerException(message: 'O telefone é obrigatório.');
+    } else if (!format.hasMatch(phone)) {
+      throw DomainLayerException(
+          message: 'Formato inválido. O formato deve ser (99) 9 9999-9999.');
+    }
+  }
+
+  validateEmail(String? email) {
+    if (email == null) {
+      throw DomainLayerException(message: "O email é obrigatório.");
+    } else if (!email.contains('@')) {
+      throw DomainLayerException(message: 'O email deve possuir @');
+    }
+  }
+
+  validateCity(
+    String? city,
+    String? state,
+    String? zipCode,
+    String? streetAddress,
+    String? streetAddressNumber,
+    String? complement,
+  ) {
+    if (city == null && state != null ||
+        zipCode != null ||
+        streetAddress != null ||
+        streetAddressNumber != null ||
+        complement != null) {
+      throw DomainLayerException(message: 'O campo Cidade é obrigatório.');
+    }
+  }
+
+  validateState(
+    String? city,
+    String? state,
+    String? zipCode,
+    String? streetAddress,
+    String? streetAddressNumber,
+    String? complement,
+  ) {
+    if (state == null && city != null ||
+        zipCode != null ||
+        streetAddress != null ||
+        streetAddressNumber != null ||
+        complement != null) {
+      throw DomainLayerException(message: 'O campo Estado é obrigatório.');
+    }
+  }
+}
