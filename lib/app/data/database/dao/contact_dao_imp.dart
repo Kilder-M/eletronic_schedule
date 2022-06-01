fe5d14c87b23b55fd1b35568c40586aabf643c1a
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-06-01 18:28:01 -0300

    feat: :sparkles: Cadastro de contatos finalizado.

--- lib/app/data/database/dao/contact_dao_imp.dart
+++ lib/app/data/database/dao/contact_dao_imp.dart
@@ -20,7 +20,7 @@ class ContactDAOImp implements ContactInterface {
   Future<List<Contact>> getListByName(String? name) async {
     _db = await Connection.getConnection();
     List<Map<String, dynamic>> queryResult =
-        await _db!.query('contact', where: 'name = ?',whereArgs: [name]);
+        await _db!.query('contact', where: 'name = ?', whereArgs: [name]);
     List<Contact> contactList = List.generate(queryResult.length, (index) {
       var row = queryResult[index];
       return Contact.fromMap(row);
@@ -32,7 +32,7 @@ class ContactDAOImp implements ContactInterface {
   Future<List<Contact>> getListByPhone(String phone) async {
     _db = await Connection.getConnection();
     List<Map<String, dynamic>> queryResult =
-        await _db!.query('contact', where: 'phone = ?',whereArgs: [phone]);
+        await _db!.query('contact', where: 'phone = ?', whereArgs: [phone]);
     List<Contact> contactList = List.generate(queryResult.length, (index) {
       var row = queryResult[index];
       return Contact.fromMap(row);
@@ -51,28 +51,32 @@ class ContactDAOImp implements ContactInterface {
 
   @override
   save(Contact contact) async {
-    _db = await Connection.getConnection();
-    String sql;
-    if (contact.id == null) {
-      sql =
-          '''INSERT INTO contact(address_id,image_url,email,phone,name)VALUES(?,?,?,?,?)''';
-      _db!.rawInsert(sql, [
-        contact.addressId,
-        contact.imageUrl,
-        contact.email,
-        contact.phone,
-        contact.name
-      ]);
-    } else {
-      sql =
-          '''UPDATE contact SET address_id = ?, image_url = ?, email = ? phone = ?, name = ? ''';
-      _db!.rawUpdate(sql, [
-        contact.addressId,
-        contact.imageUrl,
-        contact.email,
-        contact.phone,
-        contact.name
-      ]);
+    try {
+      _db = await Connection.getConnection();
+      String sql;
+      if (contact.id == null) {
+        sql =
+            '''INSERT INTO contact(address_id,image_url,email,phone,name)VALUES(?,?,?,?,?)''';
+        _db!.rawInsert(sql, [
+          contact.addressId,
+          contact.imageUrl,
+          contact.email,
+          contact.phone,
+          contact.name
+        ]);
+      } else {
+        sql =
+            '''UPDATE contact SET address_id = ?, image_url = ?, email = ? phone = ?, name = ? ''';
+        _db!.rawUpdate(sql, [
+          contact.addressId,
+          contact.imageUrl,
+          contact.email,
+          contact.phone,
+          contact.name
+        ]);
+      }
+    } catch (e) {
+      throw e.toString();
     }
   }
 }
--- lib/app/modules/home/controllers/contact_navigator_bar_item_controller.dart
+++ lib/app/modules/home/controllers/contact_navigator_bar_item_controller.dart
@@ -1,14 +1,16 @@
+import 'package:eletronic_schedule/app/domain/entities/contact_entity.dart';
+import 'package:eletronic_schedule/app/domain/services/contact_service.dart';
 import 'package:get/get.dart';
 
 class ContactNavigatorBarItemController extends GetxController {
+  final _contactService = ContactService();
+   var contactList = Future.value(<Contact>[]).obs;
 
-  // var contactList = Future.value(<Contact>[]).obs;
+  ContactNavigatorBarItemController() {
+    getContactList();
+  }
 
-  // HomeController() {
-  //   getContactList();
-  // }
-
-  // getContactList() {
-  //   contactList.value = _contactDAOImp.getList();
-  // }
+  Future<List<Contact>>getContactList() {
+    return contactList.value = _contactService.getList();
+  }
 }
