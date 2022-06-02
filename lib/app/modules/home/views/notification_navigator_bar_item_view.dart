8dfda7f0b81cba483636836d03b15a3e88c08c54
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-06-02 17:20:26 -0300

    feat: :sparkles: integração da tela de listagem de notificação com o banco.

--- lib/app/modules/home/views/notification_navigator_bar_item_view.dart
+++ lib/app/modules/home/views/notification_navigator_bar_item_view.dart
@@ -1,42 +1,56 @@
+import 'package:eletronic_schedule/app/domain/entities/custom_notification.dart';
+import 'package:eletronic_schedule/app/modules/home/controllers/notification_navigator_bar_item_controller.dart';
+import 'package:eletronic_schedule/app/modules/notification_form/views/notification_form_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class NotificationNavigatorBarItemView extends GetView {
+class NotificationNavigatorBarItemView
+    extends GetView<NotificationNavigatorBarItemController> {
   const NotificationNavigatorBarItemView({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
+    var _controller = Get.put(NotificationNavigatorBarItemController());
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: Colors.pink,
-            child: const Icon(Icons.add)),
+          onPressed: () {
+            Get.to(
+              const NotificationFormView(),
+            );
+          },
+          backgroundColor: Colors.pink,
+          child: const Icon(Icons.add)),
       body: Padding(
-        padding: const EdgeInsets.only(top:40.0),
+        padding: const EdgeInsets.only(top: 40.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Padding(
-              padding: EdgeInsets.only(left:20.0),
+              padding: EdgeInsets.only(left: 20.0),
               child: Text(
                 'Lembretes',
                 style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
               ),
             ),
             Expanded(
-              child: ListView.builder(
-                  itemCount: 2,
-                  itemBuilder: (context, snapshot) {
-                    return const ListTile(
-                      title: Text(
-                        '5:00am',
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                      ),
-                      subtitle: Text('ter,jun'),
-                      leading:
-                          CircleAvatar(backgroundColor: Colors.blue, radius: 30),
-                    );
+              child: FutureBuilder<List<CustomNotification>>(
+                  future: _controller.customNotificationList.value,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else {
+                      var customNotificationList =
+                          snapshot.data as List<CustomNotification>;
+                      return ListView.builder(
+                        itemCount: customNotificationList.length,
+                        itemBuilder: (context, index) {
+                          var customNotification =
+                              customNotificationList[index];
+                          return listTileWidget(
+                              time: customNotification.time, subtitle: customNotification.body);
+                        },
+                      );
+                    }
                   }),
             ),
           ],
@@ -44,4 +58,24 @@ class NotificationNavigatorBarItemView extends GetView {
       ),
     );
   }
+
+  Widget listTileWidget({String? time, String? subtitle}) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(
+            '$time',
+            style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+          ),
+          subtitle: Text(subtitle ?? ''),
+        ),
+        Container(
+          width: double.infinity,
+          height: 0.5,
+          color: Colors.grey,
+        )
+      ],
+    );
+  }
 }
+
